2ae57bc8274224ff9ed31e8796bd149fd38195c4
Josh Hehir <hehir24@gmail.com>
2022-09-21 16:05:21 +1000

    scoring

--- Assets/Scripts/Scoring/Score.cs
+++ Assets/Scripts/Scoring/Score.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
 
     int toalScore;
+    public TextMeshProUGUI textScore;
 
 
     // Start is called before the first frame update
